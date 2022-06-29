6d0f1e2ad1c37e8f1c2990c128ebb56f3dfc17e6
Fintisov Vadim <fintisov.vadim@gmail.com>
2022-06-29 17:27:28 +0300

    done homework practical lesson. Lesson #72

--- js+react/JavaScript/project_3/js/script.js
+++ js+react/JavaScript/project_3/js/script.js
@@ -1,93 +1,143 @@
 document.addEventListener("DOMContentLoaded", () => {
-    //___ Tabs ___
+        //___ Tabs ___
 
-    let tabs = document.querySelectorAll(".tabheader__item"),
-        tabsParent = document.querySelector(".tabheader__items"),
-        tabsContent = document.querySelectorAll(".tabcontent");
+        let tabs = document.querySelectorAll(".tabheader__item"),
+            tabsParent = document.querySelector(".tabheader__items"),
+            tabsContent = document.querySelectorAll(".tabcontent");
 
-    function hiddenTabContent() {
-        tabsContent.forEach(elem => {
-            elem.classList.add("hide")
-            elem.classList.remove("show", "fade");
+        function hiddenTabContent() {
+            tabsContent.forEach(elem => {
+                elem.classList.add("hide")
+                elem.classList.remove("show", "fade");
+            })
+
+            tabs.forEach(elem => {
+                elem.classList.remove("tabheader__item_active")
+            })
+        }
+
+        function showTabContent(i = 0) {
+            tabsContent[i].classList.add("show", "fade");
+            tabs[i].classList.add("tabheader__item_active");
+        }
+
+        hiddenTabContent();
+
+        showTabContent()
+
+        tabsParent.addEventListener("click", event => {
+            if (event.target && event.target.classList.contains("tabheader__item")) {
+                tabs.forEach((elem, i) => {
+                    if (elem == event.target) {
+                        hiddenTabContent();
+                        showTabContent(i);
+                    }
+                })
+            }
         })
 
-        tabs.forEach(elem => {
-            elem.classList.remove("tabheader__item_active")
-        })
-    }
+        //___ Timer ___
 
-    function showTabContent(i = 0) {
-        tabsContent[i].classList.add("show", "fade");
-        tabs[i].classList.add("tabheader__item_active");
-    }
+        let deadLine = "2023-06-01";
 
-    hiddenTabContent();
+        function getTime(date) {
+            let millisecond = Date.parse(date) - Date.parse(new Date()),
+                seconds = Math.floor((millisecond / 1000) % 60),
+                minutes = Math.floor((millisecond / (1000 * 60) % 60)),
+                hours = Math.floor((millisecond / (1000 * 60 * 60) % 24)),
+                day = Math.floor((millisecond / (1000 * 60 * 60 * 24)));
 
-    showTabContent()
+            if (millisecond <= 0) {
+                seconds = 0;
+                minutes = 0;
+                hours = 0;
+                day = 0;
+            }
 
-    tabsParent.addEventListener("click", event => {
-        if (event.target && event.target.classList.contains("tabheader__item")) {
-            tabs.forEach((elem, i) => {
-                if (elem == event.target) {
-                    hiddenTabContent();
-                    showTabContent(i);
+            return {
+                "millisecond": millisecond,
+                "seconds": seconds,
+                "minutes": minutes,
+                "hours": hours,
+                "day": day,
+            }
+        }
+
+        function getZero(num) {
+            if (num >= 0 && num < 10) {
+                return `0${num}`;
+            } else {
+                return num;
+            }
+        }
+
+        function setTimerBlock(selector, endTime) {
+            const t = document.querySelector(selector),
+                seconds = t.querySelector("#seconds"),
+                minutes = t.querySelector("#minutes"),
+                hours = t.querySelector("#hours"),
+                days = t.querySelector("#days"),
+                timerInterval = setInterval(updateClock, 1000);
+
+            updateClock()
+
+            function updateClock() {
+                const time = getTime(endTime);
+                days.innerText = getZero(time.day);
+                hours.innerText = getZero(time.hours);
+                minutes.innerText = getZero(time.minutes);
+                seconds.innerText = getZero(time.seconds);
+
+                if (time.millisecond <= 0) {
+                    clearInterval(timerInterval);
+                }
+            }
+        }
+
+        setTimerBlock(".timer", deadLine);
+
+        //___ Modal window ___
+
+
+        function modalWindow(modalBtn, modalBlock) {
+            const modalOpenBtn = document.querySelectorAll(modalBtn),
+                modalCloseBtn = document.querySelectorAll("[data-modal-close]"),
+                modal = document.querySelector(modalBlock);
+
+            function openModal() {
+                modal.classList.add("show");
+                modal.classList.remove("hidden");
+                document.querySelector("body").style.overflow = "hidden";
+            }
+
+            function closeModal() {
+                modal.classList.add("hidden");
+                modal.classList.remove("show");
+                document.querySelector("body").style.overflow = "";
+            }
+
+            modalOpenBtn.forEach(elem => {
+                elem.addEventListener("click", openModal);
+            })
+
+            modalCloseBtn.forEach(elem => {
+                elem.addEventListener("click", closeModal)
+            })
+
+            modal.addEventListener("click", (event) => {
+                if (event.target === modal && modal.classList.contains("show")) {
+                    closeModal();
+                }
+            });
+
+            document.addEventListener("keydown", (event) => {
+                if (event.code === "Escape" && modal.classList.contains("show")) {
+                    closeModal();
                 }
             })
         }
-    })
 
-    //___ Timer ___
-
-    let deadLine = "2022-06-02";
-
-    function getTime(date) {
-        let millisecond = Date.parse(date) - Date.parse(new Date()),
-            seconds = Math.floor((millisecond / 1000) % 60),
-            minutes = Math.floor((millisecond / (1000 * 60) % 60)),
-            hours = Math.floor((millisecond / (1000 * 60 * 60) % 24)),
-            day = Math.floor((millisecond / (1000 * 60 * 60 * 24)));
-
-        return {
-            "millisecond": millisecond,
-            "seconds": seconds,
-            "minutes": minutes,
-            "hours": hours,
-            "day": day,
-        }
+        modalWindow("[data-modal-open]", "#modal-window")
     }
-
-    function getZero(num) {
-        if (num >= 0 && num < 10) {
-            return `0${num}`;
-        } else {
-            return num;
-        }
-    }
-
-    function setTimerBlock(selector, endTime) {
-        const t = document.querySelector(selector),
-            seconds = t.querySelector("#seconds"),
-            minutes = t.querySelector("#minutes"),
-            hours = t.querySelector("#hours"),
-            days = t.querySelector("#days"),
-            timerInterval = setInterval(updateClock, 1000);
-
-        updateClock()
-
-        function updateClock() {
-            const time = getTime(endTime);
-
-            if (time.millisecond <= 0) {
-               return  clearInterval(timerInterval);
-            }
-
-            days.innerText = getZero(time.day);
-            hours.innerText = getZero(time.hours);
-            minutes.innerText = getZero(time.minutes);
-            seconds.innerText = getZero(time.seconds);
-        }
-    }
-
-    setTimerBlock(".timer", deadLine);
-})
+)
 
